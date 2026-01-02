88dd8464bea7ddcce19c1b360d9d13b32a3e62cd
Richard Guenther <richardguenther@fastmail.com>
2026-01-03 00:25:32 +0100

    vom server 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const os = require('os');
+const http = require("http");
+const os = require("os");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`
-      <h1>Test Deploy Works! Now</h1>
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <h1>Test Deploy Works! Now 2!</h1>
       <p>Time: ${new Date().toISOString()}</p>
       <p>Host: ${os.hostname()}</p>
     `);
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(3000, () => console.log("Server running on port 3000"));
 // test
