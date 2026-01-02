f960649904ec2ddecd5135d18781a383aec9a45a
Your Name <your.email@example.com>
2026-01-02 10:30:23 +0100

    test 6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(`
-      <h1>Test Deploy Works!</h1>
+      <h1>Test Deploy Works! Now</h1>
       <p>Time: ${new Date().toISOString()}</p>
       <p>Host: ${os.hostname()}</p>
     `);
